83f83d913dfeb84c7e0b5466fc4d6422e6e0f213
Gerald <157247952+Gerald450@users.noreply.github.com>
2026-05-17 13:38:16 -0400

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.py
@@ -0,0 +1,80 @@
+class TimeMap:
+
+    '''
+    input: set(key, val and timestamp)
+    output: get(key, timestamp_prev <= timestamp)
+
+    edge: duplicate timestamps, going down stamps
+
+    plan:
+    alice: {1: happy, 3:sad}
+    find num == timestamp or just below
+    traverse in timestamp dict
+    if find higher return prev
+    {key:{}}
+
+
+    hint: key: [(timestamp, value)]
+    store sorted
+    do binary
+    '''
+    from collections import defaultdict
+    def __init__(self):
+        self.hashmap = defaultdict(dict)
+        self.timestamps = []
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.hashmap[key].update({timestamp: value})
+        self.timestamps.append(timestamp)
+        
+        
+    def get(self, key: str, timestamp: int) -> str:
+        if key in self.hashmap:
+            if timestamp in self.hashmap[key]:
+                return self.hashmap[key][timestamp]
+
+            minNum = min(self.timestamps)
+            
+            l, r = minNum, timestamp
+            output = -1
+
+            for num in self.timestamps:
+                if (output < num < timestamp) and num in self.hashmap[key]:
+                    output = num
+                    
+            return '' if output == -1 else self.hashmap[key][output]
+
+
+
+        else:
+            return ''
+            
+          
+
+    
+    
+    
+    '''
+    timeMap.set("alice", "happy", 1)
+    {alice: {1: happy}}
+
+    timeMap.get("alice", 1); 
+    returns happy
+
+    timeMap.get("alice", 2);
+    output = 1
+    {1: happy}
+    output = 1
+    return happy
+
+
+    {key1: {10: value1}}
+    '''
+
+
+
+
+
+
+        
