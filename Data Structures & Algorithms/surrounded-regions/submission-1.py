524a2324daaa7d20ecf38ef4e260c0870baf43da
Gerald <157247952+Gerald450@users.noreply.github.com>
2026-06-15 20:17:26 -0500

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.py
@@ -0,0 +1,65 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None: 
+        '''
+        input: grid
+        output: grid
+
+        edge:o on edge, no os, empty grid, grid len is 1
+
+        plan:
+        capture unsurrounded regions and label them
+        change os to xs
+        unlabel unsarrounded regions
+
+        '''
+
+
+        rows, cols = len(board), len(board[0])
+
+        def dfs(row, col):
+            if (row < 0 or row >= rows) or (col < 0 or col >= cols) or board[row][col] != "O":
+                return
+            
+            board[row][col] = 'T'
+
+            dfs(row - 1, col)
+            dfs(row + 1, col)
+            dfs(row, col + 1)
+            dfs(row, col - 1)
+            
+
+
+        #capture unsorrounded
+        for row in range(rows):
+            for col in range(cols):
+                if row in [0, rows - 1] or col in [0, cols - 1] and board[row][col] == "O":
+                    dfs(row, col)
+
+        #change os to xs
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == 'O':
+                    board[row][col] = 'X'
+        
+        #uncapture
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == 'T':
+                    board[row][col] = 'O'
+
+
+        '''
+        [
+  ["X","X","X","X"],
+  ["X","O","O","X"],
+  ["X","X","O","X"],
+  ["X","O","X","X"]
+]
+
+        time: O(nm)
+        space: O(n) recursion depth
+        '''
+
+        
+
+        
